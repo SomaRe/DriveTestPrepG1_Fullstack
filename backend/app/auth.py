fff53189a297fb70392fbfb07d0d8f60dc322ac7
SomaRe <shivatej82@gmail.com>
2024-12-04 01:09:50 -0500

    fix: Flask jwt needs token to be string (new update)

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -20,6 +20,6 @@ def login():
     data = request.get_json()
     user = User.query.filter_by(username=data['username']).first()
     if user and user.check_password(data['password']):
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=str(user.id))
         return jsonify(access_token=access_token), 200
     return jsonify({"msg": "Bad username or password"}), 401
